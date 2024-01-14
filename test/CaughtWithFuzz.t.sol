53e7c0b9c655360011725316765f213b992462e4
Charles <azanux@gmail.com>
2024-01-14 21:13:26 +0100

    audit

--- test/CaughtWithFuzz.t.sol
+++ test/CaughtWithFuzz.t.sol
@@ -11,8 +11,14 @@ contract CaughtWithFuzzTest is Test {
         caughtWithFuzz = new CaughtWithFuzz();
     }
 
-    function testFuzz(uint256 randomNumber) public {
+    function testFuzz(uint256 randomNumber) public view {
         uint256 returnedNumber = caughtWithFuzz.doMoreMath(randomNumber);
         assert(returnedNumber != 0);
+    }
+
+    function testWithoutFuzz() public  {
+        uint256 returnedNumber = caughtWithFuzz.doMoreMath(1265);
+        console.log("result ",returnedNumber);
+        assertFalse(returnedNumber == 0);
     }
 }
